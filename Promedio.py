c8d1b121b31c979b30dfb3af57f6c73b9a715528
Arias232 <bayron5757@gmail.com>
2021-08-15 16:23:16 -0600

    Primeros archivos

--- Promedio.py
+++ Promedio.py
@@ -1,3 +1,5 @@
+print("En el siguiente ejercicio se solicitarán 3 cantidades para determinar el promedio de las mismas")
+    
 a=float(input("Primer Cantidad: "))
 b=float(input("Segunda cantidad: "))
 c=float(input("Tercer cantidad: "))
