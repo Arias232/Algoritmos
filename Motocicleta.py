00b0b2f353c7f6c220837c232bbda561785cd23e
Arias232 <bayron5757@gmail.com>
2021-08-15 16:06:25 -0600

    Primeros archivos

---
+++ Motocicleta.py
@@ -0,0 +1,6 @@
+print("Bienvenido en el siguiente programa se calculará el consumo por kilometro")
+
+a=float(input("Ingrese número de kilómetros recorridos: "))
+b=float(input("Ingrese litros de combustibles consumidos: "))
+print("El consumo por kilómetro recorrido es de: ",a/b)
+
---
+++ Promedio.py
@@ -0,0 +1,4 @@
+a=float(input("Primer Cantidad: "))
+b=float(input("Segunda cantidad: "))
+c=float(input("Tercer cantidad: "))
+print("El promedio de las cantidades anteriores es de: ", (a+b+c)/3)
